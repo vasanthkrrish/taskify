83f3b21e6573582a9633054e1844fe8d1ac1ed0d
Vasantha-idexcel <vasantha.r@idexcel.com>
2023-03-15 09:07:27 +0530

    Added Tasks Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tasks
+  root to: 'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
